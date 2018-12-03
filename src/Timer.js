126fded80965705c0ba8baa73ef3f42d50d13f67
asc5025 <albert.s.chun@gmail.com>
2018-12-02 20:55:51 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -14,7 +14,16 @@ class Timer extends Component {
 
 
   //Your code here
+  shouldComponentUpdate(nextProps, nextState){
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
 
+  componentDidUpdate() {
+
+  }
 
 
   componentDidMount() {
